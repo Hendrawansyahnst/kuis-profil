29d02c22a9172098626a4406d88e9b7d964e5958
hendrawansyah <12150113914@students.uin-suska.ac.id>
2023-04-18 12:41:40 +0700

    first commit

---
+++ src/view/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="content text-center">
+        <h1 class=""> TREE I DEA</h1>
+        <h2>Mempermudah segala urusan Design</h2>
+    </div>
+    <div class="image">
+        <div class="Projek">
+            <img src="../assets/poster.jpg" height="800px">
+        </div>
+        <div class="hasil">
+            <img src="../assets/spanduk.jpg" width="600px">
+        </div>
+    </div>
+</template>
---
+++ src/view/home.vue
@@ -0,0 +1,67 @@
+<template>
+
+    <section id = "sec-2" class = "py-5">
+      <div class = "container my-4">
+        <div class = "row gx-lg-5">
+          <div class = "col-lg-6 mb-5">
+            <img src = "../assets/IMG-20200928-WA0060.jpg" class = "img-fluid" height="600px" width="900px">
+          </div>
+          <div class = "col-lg-6">
+            <h3>About me</h3>
+            <p class = "text-muted fw-light lh-lg my-4"> Hello Gais </p>
+            <p class = "text-muted fw-light lh-lg my-4"> Nama saya Hendrawansyah Nasution, saya lahir pada tanggal Agustus 2001 di kota Pekanbaru, sekarang saya tinggal di Pekanbaru. dan profesi saya adalah mahasiswa di UIN Sultan Syarif Kasim Riau dan membuat usaha jasa design dengan ini saya ingin memperkenalkan usaha saya dan teman saya kepada bapak/ibu/teman-teman yang liat web profile ini.</p>
+            <p class="text-muted fw-light lh-lg my-4"> TERIMAKASIH </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  
+    
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+    components: {
+    }
+  }
+  </script>
+  
+  <style>
+  :root{
+    --blue: #4B65E8F4;
+    --blue-dark: #444BCCF4;
+    --light-grey: #f1f4f6;
+  }
+  
+  *{
+    font-family: 'Roboto', sans-serif;
+  }
+  .col-lg-6{
+    font-size: 20px;
+    margin-top: -700px;
+  }
+  
+  ul li{
+    list-style-type: none;
+  }
+  .container{
+    max-width: 1200px;
+  }
+  .display-1{
+    letter-spacing: 1px;
+  }
+  .bg-blue{
+    background-color: var(--blue)!important;
+  }
+  .bg-grey{
+    background-color: var(--light-grey);
+  }
+  .text-blue{
+    color: var(--blue);
+  }
+  .lh-lg{
+    line-height: 2.7!important;
+  }
+
+  </style>
